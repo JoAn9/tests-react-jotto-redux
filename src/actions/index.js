b9d8c292468c908c4d35ffe14b7da14e9835903b
JoAn <joannacalinska@gmail.com>
2020-07-10 21:47:03 +0200

    code for guessing a word

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,24 @@
+import { getLettersMatch } from '../helpers';
+
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
   GUESS_WORD: 'GUESS_WORD',
 };
 
-export const guessWord = guessWord => (dispatch, getState) => {};
+export const guessWord = guessedWord => {
+  return function(dispatch, getState) {
+    const { CORRECT_GUESS, GUESS_WORD } = actionTypes;
+    const secretWord = getState().secretWord;
+    const lettersMatch = getLettersMatch(guessedWord, secretWord);
+    dispatch({
+      type: GUESS_WORD,
+      payload: { guessedWord, lettersMatch },
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({
+        type: CORRECT_GUESS,
+      });
+    }
+  };
+};
