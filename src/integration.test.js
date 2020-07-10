6a8ff07d10d2a077e775bdd4c9c549220729bc54
JoAn <joannacalinska@gmail.com>
2020-07-10 21:46:32 +0200

    new integration tests for guessing a word

--- src/integration.test.js
+++ src/integration.test.js
@@ -26,11 +26,55 @@ describe('guessWord action dispatcher', () => {
       };
       expect(newState).toEqual(expectedState);
     });
-    test('update state correctly for successful guess', () => {});
+    test('update state correctly for successful guess', () => {
+      store.dispatch(guessWord(secretWord));
+      const newState = store.getState();
+      const expectedState = {
+        ...initialState,
+        success: true,
+        guessedWords: [
+          {
+            guessedWord: secretWord,
+            lettersMatch: 5,
+          },
+        ],
+      };
+      expect(newState).toEqual(expectedState);
+    });
   });
 
   describe('some guessed words', () => {
-    test('update state correctly for unsuccessful guess', () => {});
-    test('update state correctly for successful guess', () => {});
+    const guessedWords = [{ guessedWord: 'scrum', lettersMatch: 1 }];
+    const initialState = { guessedWords, secretWord };
+    let store;
+    beforeEach(() => {
+      store = storeFactory(initialState);
+    });
+    test('update state correctly for unsuccessful guess', () => {
+      store.dispatch(guessWord(notCorrectGuess));
+      const newState = store.getState();
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessedWords: [
+          ...guessedWords,
+          { guessedWord: notCorrectGuess, lettersMatch: 3 },
+        ],
+      };
+      expect(newState).toEqual(expectedState);
+    });
+    test('update state correctly for successful guess', () => {
+      store.dispatch(guessWord(secretWord));
+      const newState = store.getState();
+      const expectedState = {
+        ...initialState,
+        success: true,
+        guessedWords: [
+          ...guessedWords,
+          { guessedWord: secretWord, lettersMatch: 5 },
+        ],
+      };
+      expect(newState).toEqual(expectedState);
+    });
   });
 });
