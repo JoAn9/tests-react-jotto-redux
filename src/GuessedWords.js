c6345efc11dec39e1fa767ef8acb1e8513e9cc6f
JoAn <joannacalinska@gmail.com>
2020-08-01 23:09:26 +0200

    GuessedWords basic tests & code

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -1,7 +1,38 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function GuessedWords() {
-  return <div>content</div>;
+function GuessedWords({ guessedWords }) {
+  const content =
+    guessedWords.length > 0 ? (
+      <table data-test="table-guessedWords" className="table table-sm">
+        <thead className="thead-light">
+          <tr>
+            <th>Guess</th>
+            <th>Matching Letters</th>
+          </tr>
+        </thead>
+        <tbody>
+          {guessedWords.map((word, index) => (
+            <tr data-test="table-row" key={index}>
+              <td>{word.guessedWord}</td>
+              <td>{word.lettersMatch}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    ) : (
+      <span data-test="instructions">Try to guess the secret word!</span>
+    );
+  return <div data-test="component-guessedWords">{content}</div>;
 }
+
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      lettersMatch: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
 
 export default GuessedWords;
---
+++ src/GuessedWords.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import GuessedWords from './GuessedWords';
+
+const setup = (props = {}) => {
+  const wrapper = shallow(<GuessedWords {...props} />);
+  return wrapper;
+};
+
+describe('if there are no words guessed', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+
+  test('renders without error', () => {
+    const component = wrapper.find('[data-test="component-guessedWords"]');
+    expect(component.length).toBe(1);
+  });
+  test('renders instructions to guess a word', () => {
+    const instructions = wrapper.find('[data-test="instructions"]');
+    expect(instructions.text().length).not.toBe(0);
+  });
+});
+describe('if there are words guessed', () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: 'beer', lettersMatch: 1 },
+    { guessedWord: 'wine', lettersMatch: 0 },
+  ];
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+  test('renders without error', () => {
+    const component = wrapper.find('[data-test="component-guessedWords"]');
+    expect(component.length).toBe(1);
+  });
+  test('renders "guessed words" section', () => {
+    const table = wrapper.find('[data-test="table-guessedWords"]');
+    expect(table.length).toBe(1);
+  });
+  test('correct number of guessed words', () => {
+    const row = wrapper.find('[data-test="table-row"]');
+    expect(row.length).toBe(guessedWords.length);
+  });
+});
